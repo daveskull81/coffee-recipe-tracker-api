ad5e943bc037e736c2c8f872fec15b86afee2394
dAVE Inden <davidinden@gmail.com>
2019-11-25 14:50:09 -0800

    fix migration error and run to create tables

--- data/migrations/20191125112220_create_tables.js
+++ data/migrations/20191125112220_create_tables.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', tbl => {
       tbl.increments();
-      tbl.string('username', 255).notNullable().notUnique();
+      tbl.string('username', 255).notNullable().unique();
       tbl.string('password', 255).notNullable();
   })
   .createTable('coffees', tbl => {
