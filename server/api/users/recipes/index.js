136c9efb053f90fa8a53918a14e52ee57863af17
dAVE Inden <davidinden@gmail.com>
2019-11-26 14:36:14 -0800

    middleware to add recipe id to req object

--- server/api/users/recipes/index.js
+++ server/api/users/recipes/index.js
@@ -1,11 +1,12 @@
 const recipes = require('express').Router();
 const notes = require('./notes');
+const { addRecipeId } = require('../../../middleware/custom');
 
 recipes.get('/', (req, res) => {
     res.status(200).json({ message: 'Response from /api/users/:userId/recipes'});
 });
 
 
-recipes.use('/:recipeId/notes', notes);
+recipes.use('/:recipeId/notes', addRecipeId, notes);
 
 module.exports = recipes;
--- server/middleware/custom/index.js
+++ server/middleware/custom/index.js
@@ -1,9 +1,11 @@
 const validateUser = require('./validateUser');
 const addUserId = require('./addUserId');
 const validateJWT = require('./validateJWT');
+const addRecipeId = require('./addRecipeId');
 
 module.exports = {
     validateUser,
     addUserId,
-    validateJWT
+    validateJWT,
+    addRecipeId
 };
