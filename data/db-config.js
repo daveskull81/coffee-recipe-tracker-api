abd6c0a0ed26166343a41076406a564f08ccd721
dAVE Inden <davidinden@gmail.com>
2019-11-26 11:23:43 -0800

    configure global middleware for sessions

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
 const config = require('../knexfile');
-const dbEnv = process.env.DB_ENV;
+const dbEnv = process.env.NODE_ENV;
 
 module.exports = knex(config[dbEnv]);
--- server/middleware/global/index.js
+++ server/middleware/global/index.js
@@ -1,9 +1,34 @@
 const json = require('express').json;
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const SessionStorage = require('connect-session-knex')(session);
+const db = require('../../../data/db-config');
+
+const sessionStore = new SessionStorage({
+    knex: db,
+    clearInterval: process.env.SESSION_STORAGE_CLEAR_INTERVAL,
+    tablename: 'user_sessions',
+    sidfieldname: 'id',
+    createTable: true
+});
+
+const sessionConfig = {
+    name: 'coffee-recipe-tracker',
+    secret: process.env.SESSION_COOKIE_SECRET,
+    cookie: {
+        maxAge: process.env.SESSION_MAX_AGE,
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore
+};
 
 module.exports = server => {
     server.use(helmet());
     server.use(cors());
     server.use(json());
+    server.use(session(sessionConfig));
 };
