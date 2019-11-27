6703390d4a197c08d24ea3dd98891540f7057440
dAVE Inden <davidinden@gmail.com>
2019-11-27 09:25:48 -0800

    fix method object validation middleware to stop request process correctly on error

--- server/middleware/custom/validateMethodObject.js
+++ server/middleware/custom/validateMethodObject.js
@@ -1,10 +1,9 @@
 module.exports = (req, res, next) => {
     if (req.body.id) {
         res.status(400).json({ message: 'The method object cannot contain the property id. '});
-    }
-
-    if (!req.body.name) {
+    } else if (!req.body.name) {
         res.status(400).json({ message: 'The method object is missing the name property.' });
-    }
-    next();
+    } else {
+        next();
+    };
 };
