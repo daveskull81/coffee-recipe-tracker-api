beb1c934f87bd5a1cd2232788ef44e169e7adf16
dAVE Inden <davidinden@gmail.com>
2019-11-27 10:16:06 -0800

    implement util to remove properties as needed and add to methods endpoints

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 const convertNumBoolean = require('./convertNumBoolean');
 const generateJWT = require('./generateJWT');
 const DB = require('./db-helpers');
+const removeProperties = require('./removeProperties');
 
 module.exports = {
     convertNumBoolean,
     generateJWT,
-    DB
+    DB,
+    removeProperties
 };
