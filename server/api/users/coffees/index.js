455965973fca389207aa101c34cf8a696019d84f
dAVE Inden <davidinden@gmail.com>
2019-11-27 12:13:06 -0800

    complete coffees endpoints with validation

--- server/api/users/coffees/index.js
+++ server/api/users/coffees/index.js
@@ -1,5 +1,6 @@
 const coffees = require('express').Router();
 const { DB, removeProperties } = require('../../../../utils');
+const { validateId, validateCoffeeObject } = require('../../../middleware/custom');
 
 coffees.get('/', (req, res) => {
     DB.search('coffees', 'user_id', req.userId)
@@ -10,11 +11,16 @@ coffees.get('/', (req, res) => {
         .catch(err => res.status(500).json({ message: "There was an error getting the coffees.", error: err.message }));
 });
 
-coffees.post('/', (req, res) => {
-    res.status(200).json({ message: 'Response from /api/users/:userId/coffees'});
+coffees.post('/', validateCoffeeObject, (req, res) => {
+    DB.add('coffees', req.body)
+        .then(result => {
+            const newCoffee = removeProperties(result, ['user_id']);
+            res.status(200).json(newCoffee);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error adding the coffee', error: err.message }));
 });
 
-coffees.get('/:coffeeId', (req, res) => {
+coffees.get('/:coffeeId', validateId('coffees', 'coffeeId'), (req, res) => {
     DB.findById('coffees', req.params.coffeeId)
         .then(result => {
             const coffee = removeProperties(result, ['user_id']);
@@ -23,12 +29,22 @@ coffees.get('/:coffeeId', (req, res) => {
         .catch(err => res.status(500).json({ message: 'There was an error getting the coffee', error: err.message }));
 });
 
-coffees.put('/:coffeeId', (req, res) => {
-    res.status(200).json({ message: 'Response from /api/users/:userId/coffees'});
+coffees.put('/:coffeeId', validateCoffeeObject, validateId('coffees', 'coffeeId'), (req, res) => {
+    DB.update('coffees', req.body, req.params.coffeeId)
+        .then(result => {
+            const coffee = removeProperties(result, ['user_id']);
+            res.status(200).json(coffee);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error updating the coffee.', error: err.message }));
 });
 
-coffees.delete('/:coffeeId', (req, res) => {
-    res.status(200).json({ message: 'Response from /api/users/:userId/coffees'});
+coffees.delete('/:coffeeId', validateId('coffees', 'coffeeId'), (req, res) => {
+    DB.remove('coffees', req.params.coffeeId)
+        .then(result => {
+            const deletedCoffee = removeProperties(result, ['user_id']);
+            res.status(200).json(deletedCoffee);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error deleting the coffee.', error: err.message }));
 });
 
 module.exports = coffees;
--- server/middleware/custom/index.js
+++ server/middleware/custom/index.js
@@ -3,11 +3,13 @@ const validateJWT = require('./validateJWT');
 const validateMethodObject = require('./validateMethodObject');
 const validateId = require('./validateId');
 const addParamToReqObj = require('./addParamToReqObj');
+const validateCoffeeObject = require('./validateCoffeeObject');
 
 module.exports = {
     validateUserObject,
     validateJWT,
     validateMethodObject,
     validateId,
-    addParamToReqObj
+    addParamToReqObj,
+    validateCoffeeObject
 };
