095e83f3967bf10cde4a8d75c6fc57a28e6e87ba
dAVE Inden <davidinden@gmail.com>
2019-11-27 12:41:23 -0800

    update migration for recipe creation and updte seeds

--- data/seeds/04-recipes.js
+++ data/seeds/04-recipes.js
@@ -1,10 +1,12 @@
 exports.seed = function(knex) {
   return knex('recipes').insert([
     {
+      name: 'Recipe 1',
       coffee_weight: 25.1,
       total_weight: 375,
       bloom: 1,
       bloom_time: 30,
+      bloom_weight: 75,
       time_minutes: 2,
       time_seconds: 30,
       grind_size: 'Fine',
@@ -15,10 +17,12 @@ exports.seed = function(knex) {
       coffee_id: 1
     },
     {
+      name: 'Recipe 2',
       coffee_weight: 50.1,
       total_weight: 750,
       bloom: 1,
       bloom_time: 45,
+      bloom_weight: 150,
       time_minutes: 3,
       time_seconds: 30,
       grind_size: 'Medium',
@@ -29,10 +33,12 @@ exports.seed = function(knex) {
       coffee_id: 2
     },
     {
+      name: 'Recipe 3',
       coffee_weight: 50.1,
       total_weight: 750,
       bloom: 0,
       bloom_time: null,
+      bloom_weight: null,
       time_minutes: 6,
       time_seconds: 00,
       grind_size: 'medium-coarse',
