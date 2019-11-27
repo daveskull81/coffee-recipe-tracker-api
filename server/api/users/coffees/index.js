7a2320933e5c38405d62c7de42c8e49356ffa48f
dAVE Inden <davidinden@gmail.com>
2019-11-27 11:21:09 -0800

    complete get all and get by id on coffees endpoints

--- server/api/users/coffees/index.js
+++ server/api/users/coffees/index.js
@@ -1,6 +1,33 @@
 const coffees = require('express').Router();
+const { DB, removeProperties } = require('../../../../utils');
 
 coffees.get('/', (req, res) => {
+    DB.search('coffees', 'user_id', req.userId)
+        .then(results => {
+            const coffees = removeProperties(results, ['user_id']);
+            res.status(200).json(coffees);
+        })
+        .catch(err => res.status(500).json({ message: "There was an error getting the coffees.", error: err.message }));
+});
+
+coffees.post('/', (req, res) => {
+    res.status(200).json({ message: 'Response from /api/users/:userId/coffees'});
+});
+
+coffees.get('/:coffeeId', (req, res) => {
+    DB.findById('coffees', req.params.coffeeId)
+        .then(result => {
+            const coffee = removeProperties(result, ['user_id']);
+            res.status(200).json(coffee);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error getting the coffee', error: err.message }));
+});
+
+coffees.put('/:coffeeId', (req, res) => {
+    res.status(200).json({ message: 'Response from /api/users/:userId/coffees'});
+});
+
+coffees.delete('/:coffeeId', (req, res) => {
     res.status(200).json({ message: 'Response from /api/users/:userId/coffees'});
 });
 
