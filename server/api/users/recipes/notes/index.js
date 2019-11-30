48ffc358340220fb97e9da98d39ac0ccd365ab96
dAVE Inden <davidinden@gmail.com>
2019-11-30 10:55:47 -0800

    setup notes endpoints with middleware for validation

--- server/api/users/recipes/notes/index.js
+++ server/api/users/recipes/notes/index.js
@@ -1,6 +1,24 @@
 const notes = require('express').Router();
+const { DB, removeProperties } = require('../../../../../utils');
+const { validateId, validateNoteObject } = require('../../../../middleware/custom');
 
 notes.get('/', (req, res) => {
+    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+});
+
+notes.get('/:noteId', validateId('notes', 'noteId'), (req, res) => {
+    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+});
+
+notes.post('/', validateNoteObject, (req, res) => {
+    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+});
+
+notes.put('/:noteId', validateNoteObject, validateId('notes', 'noteId'), (req, res) => {
+    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+});
+
+notes.delete('/:noteId', validateId('notes', 'noteId'), (req, res) => {
     res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
 });
 
--- server/middleware/custom/index.js
+++ server/middleware/custom/index.js
@@ -5,6 +5,7 @@ const validateId = require('./validateId');
 const addParamToReqObj = require('./addParamToReqObj');
 const validateCoffeeObject = require('./validateCoffeeObject');
 const validateRecipeObject = require('./validateRecipeObject');
+const validateNoteObject = require('./validateNoteObject');
 
 module.exports = {
     validateUserObject,
@@ -13,5 +14,6 @@ module.exports = {
     validateId,
     addParamToReqObj,
     validateCoffeeObject,
-    validateRecipeObject
+    validateRecipeObject,
+    validateNoteObject
 };
