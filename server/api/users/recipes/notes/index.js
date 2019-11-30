77e86b70bcb1c7d59cc8429b063c6386c239d093
dAVE Inden <davidinden@gmail.com>
2019-11-30 11:29:25 -0800

    implement notes routes for full CRUD actions

--- server/api/users/recipes/notes/index.js
+++ server/api/users/recipes/notes/index.js
@@ -3,23 +3,48 @@ const { DB, removeProperties } = require('../../../../../utils');
 const { validateId, validateNoteObject } = require('../../../../middleware/custom');
 
 notes.get('/', (req, res) => {
-    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+    DB.search('notes', 'recipe_id', req.recipeId)
+        .then(results => {
+            const notes = removeProperties(results, ['recipe_id']);
+            res.status(200).json(notes);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error getting the notes.', error: err.message }));
 });
 
 notes.get('/:noteId', validateId('notes', 'noteId'), (req, res) => {
-    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+    DB.findById('notes', req.params.noteId)
+        .then(result => {
+            const note = removeProperties(result, ['recipe_id']);
+            res.status(200).json(note);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error getting the note.', error: err.message }));
 });
 
 notes.post('/', validateNoteObject, (req, res) => {
-    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+    DB.add('notes', req.body)
+        .then(result => {
+            const newNote = removeProperties(result, ['recipe_id']);
+            res.status(201).json(newNote);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error adding the note.', error: err.message }));
 });
 
 notes.put('/:noteId', validateNoteObject, validateId('notes', 'noteId'), (req, res) => {
-    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+    DB.update('notes', req.body, req.params.noteId)
+        .then(result => {
+            const note = removeProperties(result, ['recipe_id']);
+            res.status(200).json(note);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error updating the note.', error: err.message }));
 });
 
 notes.delete('/:noteId', validateId('notes', 'noteId'), (req, res) => {
-    res.status(200).json({ message: 'Response from /api/users/:userId/recipes/:recipeId/notes' });
+    DB.remove('notes', req.params.noteId)
+        .then(result => {
+            const deletedNote = removeProperties(result, ['recipe_id']);
+            res.status(200).json(deletedNote);
+        })
+        .catch(err => res.status(500).json({ message: 'There was an error deleting the note.', error: err.message }));
 });
 
 
