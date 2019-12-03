29725e16e009a0ae39877a0f5b4027fbc9d14644
dAVE Inden <davidinden@gmail.com>
2019-12-02 16:36:26 -0800

    update recipes endpoints to convert boolean on bloom property as expected

--- server/api/users/recipes/index.js
+++ server/api/users/recipes/index.js
@@ -1,12 +1,22 @@
 const recipes = require('express').Router();
 const notes = require('./notes');
-const { DB, removeProperties } = require('../../../../utils');
+const { DB, removeProperties, convertNumBoolean } = require('../../../../utils');
 const { addParamToReqObj, validateId, validateRecipeObject } = require('../../../middleware/custom');
 
 recipes.get('/', (req, res) => {
     DB.search('recipes', 'user_id', req.userId)
         .then(results => {
-            const recipes = removeProperties(results, ['user_id']);
+            const recipesWithNoUserId = removeProperties(results, ['user_id']);
+            const recipes = recipesWithNoUserId.map(recipe => {
+                if (recipe.bloom === null) {
+                    return recipe;
+                } else {
+                    return {
+                        ...recipe,
+                        bloom : convertNumBoolean(recipe.bloom)
+                    };
+                };
+            });
             res.status(200).json(recipes);
         })
         .catch(err => res.status(500).json({ message: "There was an error getting the recipes.", error: err.message }));
@@ -15,25 +25,70 @@ recipes.get('/', (req, res) => {
 recipes.get('/:recipeId', validateId('recipes', 'recipeId'), (req, res) => {
     DB.findById('recipes', req.params.recipeId)
         .then(results => {
-            const recipe = removeProperties(results, ['user_id']);
+            const recipeWithNoUserId = removeProperties(results, ['user_id']);
+            let recipe;
+            if (recipeWithNoUserId.bloom === null) {
+                recipe = recipeWithNoUserId;
+            } else {
+                recipe = {
+                    ...recipeWithNoUserId,
+                    bloom: convertNumBoolean(recipeWithNoUserId.bloom)
+                };
+            };
             res.status(200).json(recipe);
         })
         .catch(err => res.status(500).json({ message: 'There was an error getting the recipe.', error: err.message }));
 });
 
 recipes.post('/', validateRecipeObject, (req, res) => {
-    DB.add('recipes', req.body)
+    let newRecipe;
+    if (req.body.bloom) {
+        newRecipe = {
+                ...req.body,
+                bloom: convertNumBoolean(req.body.bloom)
+            }
+    } else {
+        newRecipe = req.body;
+    };
+    DB.add('recipes', newRecipe)
         .then(result => {
-            const newRecipe = removeProperties(result, ['user_id']);
+            const newRecipeWithNoUserId = removeProperties(result, ['user_id']);
+            let newRecipe;
+            if (newRecipeWithNoUserId.bloom === null) {
+                newRecipe = newRecipeWithNoUserId;
+            } else {
+                newRecipe = {
+                    ...newRecipeWithNoUserId,
+                    bloom: convertNumBoolean(newRecipeWithNoUserId.bloom)
+                };
+            };
             res.status(201).json(newRecipe);
         })
         .catch(err => res.status(500).json({ message: 'There was an error adding the recipe.', error: err.message }));
 });
 
 recipes.put('/:recipeId', validateRecipeObject, validateId('recipes', 'recipeId'), (req, res) => {
-    DB.update('recipes', req.body, req.params.recipeId)
+    let updatedRecipe;
+    if (req.body.bloom) {
+        updatedRecipe = {
+            ...req.body,
+            bloom: convertNumBoolean(req.body.bloom)
+        };
+    } else {
+        updatedRecipe = req.body;
+    };
+    DB.update('recipes', updatedRecipe, req.params.recipeId)
         .then(result => {
-            const recipe = removeProperties(result, ['user_id']);
+            const recipeWithNoUserId = removeProperties(result, ['user_id']);
+            let recipe;
+            if (recipeWithNoUserId.bloom === null) {
+                recipe = recipeWithNoUserId;
+            } else {
+                recipe = {
+                    ...recipeWithNoUserId,
+                    bloom: convertNumBoolean(recipeWithNoUserId.bloom)
+                };
+            };
             res.status(200).json(recipe);
         })
         .catch(err => res.status(500).json({ message: 'There was an error updating the recipe.', error: err.message }));
@@ -42,7 +97,16 @@ recipes.put('/:recipeId', validateRecipeObject, validateId('recipes', 'recipeId'
 recipes.delete('/:recipeId', validateId('recipes', 'recipeId'), (req, res) => {
     DB.remove('recipes', req.params.recipeId)
         .then(result => {
-            const deletedCoffee = removeProperties(result, ['user_id']);
+            const deletedCoffeeWithNoUserId = removeProperties(result, ['user_id']);
+            let deletedCoffee;
+            if (deletedCoffeeWithNoUserId.bloom === null) {
+                deletedCoffee = deletedCoffeeWithNoUserId
+            } else {
+                deletedCoffee = {
+                    ...deletedCoffeeWithNoUserId,
+                    bloom: convertNumBoolean(deletedCoffeeWithNoUserId.bloom)
+                };
+            };
             res.status(200).json(deletedCoffee);
         })
         .catch(err => res.status(500).json({ message: 'There was an error deleting the recipe.', error: err.message }));
