e9620fbbcba41a974c1d804f0d4fb05289758111
dAVE Inden <davidinden@gmail.com>
2019-12-15 10:39:01 -0800

    update validateId middleware to provide more info on the specific failure

--- server/middleware/custom/validateId.js
+++ server/middleware/custom/validateId.js
@@ -5,11 +5,11 @@ module.exports = (tbl, param) => {
         DB.findById(tbl, req.params[param])
           .then(item => {
               if (!item) {
-                  res.status(404).json({ message: 'Invalid Id. Item not found.' });
+                  res.status(404).json({ message: `Invalid ${param}. Not found.` });
               } else {
                   next();
               };
           })
-          .catch(err => res.status(500).json({ message: 'There was an error finding the item.', error: err.message }));
+          .catch(err => res.status(500).json({ message: `There was an error finding the item using the given ${param}`, error: err.message }));
       };
 };
